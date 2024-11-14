2e127f0664acbb97c285ab13edae865201f31858
Yeisson <yeisson.osorno@talent.com>
2024-11-14 00:33:10 -0500

    Implemented mutate to update workflow into BD

--- app/workflow/_components/topbar/SaveBtn.tsx
+++ app/workflow/_components/topbar/SaveBtn.tsx
@@ -1,17 +1,34 @@
 "use client"
+import { UpdateWorkflow } from '@/actions/workflows/updateWorkflow';
 import { Button } from '@/components/ui/button'
+import { useMutation } from '@tanstack/react-query';
 import { useReactFlow } from '@xyflow/react';
 import { CheckIcon } from 'lucide-react'
 import React from 'react'
+import { toast } from 'sonner';
 
 export default function SaveBtn({workflowId}:{workflowId:string}) {
   const {toObject} = useReactFlow();
+  const saveMutation = useMutation({
+    mutationFn:UpdateWorkflow,
+    onSuccess: () => {
+      toast.success("Workflow saved",{id:"save-workflow"});
+    },
+    onError: () => {
+      toast.error("Something went wrong",{id:"save-workflow"});
+    }
+  })
   return (
     <Button 
     variant="outline" 
     className='flex items-center gap-2' 
     onClick={()=>{
-      console.log("@FLOW",workflowId)
+      const workflowDefinition = JSON.stringify(toObject());
+      toast.loading("Saving workflow...",{id:"save-workflow"});
+      saveMutation.mutate({
+        id:workflowId,
+        definition:workflowDefinition
+      });
     }}
     > 
       <CheckIcon size={16} className='stroke-green-400'/>
