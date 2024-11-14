4b0b978a9b125bbf6bc95d0cc9f4026fa890497a
Yeisson <yeisson.osorno@talent.com>
2024-11-14 01:21:31 -0500

    Add new task page_to_html and task menu

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ReactFlowProvider } from '@xyflow/react'
 import FlowEditor from './FlowEditor'
 import Topbar from './topbar/Topbar'
+import TaskMenu from './TaskMenu'
 
 export default function Editor({workflow}:{workflow:Workflow}) {
   return (
@@ -10,6 +11,7 @@ export default function Editor({workflow}:{workflow:Workflow}) {
       <div className='flex flex-col h-full w-full overflow-hidden'>
         <Topbar WorkflowId={workflow.id} title='Workflow editor' subtitle={workflow.name}></Topbar>
         <section className="flex h-full overflow-auto">
+          <TaskMenu/>
           <FlowEditor workflow={workflow} />
         </section>
       </div>
--- app/workflow/_components/topbar/Topbar.tsx
+++ app/workflow/_components/topbar/Topbar.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 export default function Topbar({title,subtitle,WorkflowId}:Props) {
   return (
-    <header className='flex p-2 border-p-2 border-separate 
+    <header className='flex p-2 border-b-2 border-separate 
     justify-between w-full h-[60px] sticky top-0 bg-background z-10'>
       <div className="flex gap-1 flex-1">
         <TooltipWrapper content="Back">
