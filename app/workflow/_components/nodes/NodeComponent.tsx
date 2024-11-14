5255415943e79599eca24e6a944d623c834d188f
Yeisson <yeisson.osorno@talent.com>
2024-11-14 02:00:21 -0500

    Add outputs of the UI task

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -5,6 +5,7 @@ import NodeHeader from "./NodeHeader";
 import { AppNodeData } from "@/types/appNode";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import NodeInputs, { NodeInput } from "./NodeInputs";
+import NodeOutputs, { NodeOutput } from "./NodeOutputs";
 
 const NodeComponent = memo((props:NodeProps)=>{
   const nodeData = props.data as AppNodeData;
@@ -17,6 +18,11 @@ const NodeComponent = memo((props:NodeProps)=>{
           <NodeInput nodeId={props.id} key={input.name} input={input}/>
         ))}
       </NodeInputs>
+      <NodeOutputs>
+        {task.outputs.map((output,index)=>(
+          <NodeOutput key={output.name} output={output}/>
+        ))}
+      </NodeOutputs>
     </NodeCard>
   )
 })
--- app/workflow/_components/nodes/NodeInputs.tsx
+++ app/workflow/_components/nodes/NodeInputs.tsx
@@ -4,6 +4,7 @@ import { TaskParam } from '@/types/Task'
 import { Handle, Position } from '@xyflow/react'
 import React from 'react'
 import NodeParamField from './NodeParamField'
+import { ColorForHandle } from './common'
 
 export default function NodeInputs({children}:{children:React.ReactNode}) {
   return (
@@ -20,7 +21,8 @@ export function NodeInput({input,nodeId}:{input:TaskParam,nodeId:string}) {
       {!input.hideHandle &&(
         <Handle id={input.name} type='target' position={Position.Left}
         className={cn(
-          "!bg-muted-foreground !border-2 !border-background !-left-2 !w-4 !h-4"
+          "!bg-muted-foreground !border-2 !border-background !-left-2 !w-4 !h-4",
+          ColorForHandle[input.type]
         )}
         />
       )}
